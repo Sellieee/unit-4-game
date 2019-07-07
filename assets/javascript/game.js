2cd4fdc2bef132217274bbe11c78733bb52b7312
Sellieee <sellieee@hotmail.com>
2019-07-07 20:00:28 +1000

    pseudocode created

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Create individual characters stating HP, AP and CAP
+// Allow user to select enemy (only has CAP - stays constant)
+// Upon Selection, enemy moves to defender area
+// User clicks 'Attack button' to attack enemy (AP increases by base AP)
+// Decrease enemy's HP (shown under enemy picture)
+// Decrease selected character's HP as enemy counters attack
+// If enemy <=0 HP, enemy disappears from defender area
+// User to select new enemy
+// Repeat until all enemies defeated
+// User loses when selected character's HP <=0
+// Ensure unique HP, AP and CAP for each character
+// No increase in HP allowed
+Give hints to player - "First Round: Choose the enemy with the lowest CAP"
