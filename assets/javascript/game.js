bab61e91029d8d7fbee5223fc1308a5f639d129b
Sellieee <sellieee@hotmail.com>
2019-07-07 20:30:37 +1000

    html framework created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 // Create individual characters stating HP, AP and CAP
+// Obi-Wan Kenobi, Luke Skywalker, Darth Sidious, Darth Maul
 // Allow user to select enemy (only has CAP - stays constant)
 // Upon Selection, enemy moves to defender area
 // User clicks 'Attack button' to attack enemy (AP increases by base AP)
@@ -10,4 +11,8 @@
 // User loses when selected character's HP <=0
 // Ensure unique HP, AP and CAP for each character
 // No increase in HP allowed
-Give hints to player - "First Round: Choose the enemy with the lowest CAP"
+// Give hints to player - "First Round: Choose the enemy with the lowest CAP"
+// User should be able to win regardless of character selected
+
+// Side Notes:
+// If User has not selected character and clicks attack - say "No enemy here"
